fa981fcd1ba8b6148dc527923535b52d4ad8f697
Sut <odineyez@gmail.com>
2023-08-12 17:34:24 -0500

    #2

--- Grind75/2.validParentheses.js
+++ Grind75/2.validParentheses.js
@@ -25,6 +25,38 @@
 /**
  * Solution Explained:
  * We want to use the Stack Data Structure
- * Make variable to store index
+ * While looping throuh and adding the ith element of the 
+ * string s to the staack.
  * 
+ * Now we have a variable, called open that is s.length - 2
+ * We also have a variable, called close that is s.length - 1
+ * 
+ * Now we add the values of open and close 
+ * If this value are included in the potential parentheses
+ * Then we pop the stack twice.
+ * 
+ * If the stack is empty then the string s is valid. 
  */
+
+var isValid = function(s){
+  const stack = []
+  const parens = '() [] {}'
+  let i = 0 
+
+  while (i < s.length){
+    stack.push(s[i])
+    i++
+
+    let open = stack[stack.length-2]
+    let close = stack[stack.length-1]
+
+    let potentialParentheses = open + close
+
+    if (parens.includes(potentialParentheses)){
+      stack.pop()
+      stack.pop()
+    }
+  }
+
+  return stack.length === 0;
+}
