bab4a901760d662f46906b4fee9a370f39288c41
SutMa <odineyez@gmail.com>
2023-08-14 13:56:06 -0600

    GRIND75 #6

--- Grind75/5.validPalindrome.js
+++ Grind75/5.validPalindrome.js
@@ -11,6 +11,29 @@
 /**
  * Solution Explained:
  * Create regex to replace special characters with ""
- * 
+ * we have the leftIndex starting at the start of the string
+ * we have rightIndex starting at the end of the string 
+ * while loop checking to make sure the letters are the same
+ * if all the characters passes return true
  */
 
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+  s = s.replace(/[^a-z0-9]/gi,"")
+  let leftIndex = 0
+  let rightIndex = s.length-1
+
+  while(leftIndex<rightIndex){
+      if(s[leftIndex].toLowerCase() !== s[rightIndex].toLowerCase()){
+          return false
+          
+      }
+      leftIndex++
+      rightIndex--
+  }
+
+  return true
+};
---
+++ Grind75/6.invertBinaryTree.js
@@ -0,0 +1,41 @@
+/**Recursion
+ * Instruction:
+ * Given root of binary tree, invert the tree and return its root
+ */
+
+/**
+ * Example:
+ * Input: root = [4,2,7,1,3,6,9]
+ * Output: [4,7,2,9,6,3,1]
+ */
+
+/**
+ * Solution Explained:
+ * We want to use a temp variable to swap the left and right child of the parent node.
+ * Then we can recursively call the function on the left child and the right child
+ * Return the entire root
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+  if(root === null) return null
+  let temp = root.left
+  root.left = root.right
+  root.right = temp
+
+  invertTree(root.left)
+  invertTree(root.right)
+  return root
+  
+};
